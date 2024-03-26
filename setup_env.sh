32205efcd435b0a457d5f0f52b8863a7e6e6e3a5
maxwen <max.weninger@gmail.com>
2024-03-26 21:16:08 +0100

    bromite -> cromite

--- setup_env.sh
+++ setup_env.sh
@@ -4,5 +4,5 @@ CWD=$(cd "$( dirname "$0" )" && pwd)
 export PATH=$CWD/.bin:$CWD/.bin/depot_tools:$PATH
 export PYTHONPATH="$CWD/.bin/depot_tools/third_party:$PYTHONPATH"
 
-#./bromite-builder -b https://github.com/uazo/cromite  -a arm64 -u 6b4b19e9dfbb93aa414dc045bd445287977d8d7a -r 123.0.6312.58
-# apksigner sign --key testkey.pk8 --cert testkey.x509.pem /data5/bromite/bromite-builder/build/chromium/src/out/Default_123.0.6312.58/apks/SystemWebView.apk
+#./cromite-builder -u 6b4b19e9dfbb93aa414dc045bd445287977d8d7a
+# apksigner sign --key testkey.pk8 --cert testkey.x509.pem SystemWebView.apk
