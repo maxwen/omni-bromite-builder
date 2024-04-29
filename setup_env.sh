bb677fbefa68f162174a1c96f8eb943b0f1305ba
maxwen <max.weninger@gmail.com>
2024-04-29 20:32:18 +0200

    add webview-builder

--- setup_env.sh
+++ setup_env.sh
@@ -4,5 +4,10 @@ CWD=$(cd "$( dirname "$0" )" && pwd)
 export PATH=$CWD/.bin:$CWD/.bin/depot_tools:$PATH
 export PYTHONPATH="$CWD/.bin/depot_tools/third_party:$PYTHONPATH"
 
-#./cromite-builder -u 6b4b19e9dfbb93aa414dc045bd445287977d8d7a
+#./cromite-builder -u 306cfe601c2eb29d54a473203561d8d34ecaac0d
 # apksigner sign --key testkey.pk8 --cert testkey.x509.pem SystemWebView.apk
+
+#./webview-builder -u 306cfe601c2eb29d54a473203561d8d34ecaac0d
+
+
+# 306cfe601c2eb29d54a473203561d8d34ecaac0d ->  122.0.6261.120
